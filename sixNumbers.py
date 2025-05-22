b412b6a79bde417310cb91dc45053144a51913de
Ayush Yadav <ayushyadav24nov1234@gmail.com>
2025-05-22 15:23:47 +0530

    numbers

---
+++ sixNumbers.py
@@ -0,0 +1,16 @@
+x = 1
+x = 1.1
+x = 1 + 2j  # a + bi; here x will be a complex number
+
+# Standard Arithematic Operators
+print(10 + 3)  # 13
+print(10 - 3)  # 7
+print(10 * 3)  # 30
+print(10 / 3)  # 3.3333333333333335
+print(10 // 3)  # 3
+print(10 % 3)  # 1
+print(10 ** 3)  # 1000
+
+# For all these operators, we have a special operator called the augmented assignment operator
+x = 10
+x += 3  # i.e. x = x + 3
